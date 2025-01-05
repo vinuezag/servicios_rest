f3133d3063d7b0f3c9b678ade75908123beb3845
unknown <gretavinueza@gmail.com>
2025-01-05 15:32:33 -0500

    botones para dirigir a los diferentes servicios

--- app/src/main/java/com/vfe/serviciosrest/MainActivity.java
+++ app/src/main/java/com/vfe/serviciosrest/MainActivity.java
@@ -1,8 +1,13 @@
 package com.vfe.serviciosrest;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.os.Bundle;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -10,15 +15,39 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnPost, btnPut, btnAll, btnDelete;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        btnPost = findViewById(R.id.btnPost);
+        btnPut = findViewById(R.id.btnPut);
+        btnAll = findViewById(R.id.btnAll);
+        btnDelete = findViewById(R.id.btnDelete);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v.getId() == R.id.btnPost) {
+                    startActivity(new Intent(MainActivity.this, post.class));
+                } else if (v.getId() == R.id.btnPut) {
+                    startActivity(new Intent(MainActivity.this, put.class));
+                } else if (v.getId() == R.id.btnAll) {
+                    startActivity(new Intent(MainActivity.this, all.class));
+                } else if (v.getId() == R.id.btnDelete) {
+                    startActivity(new Intent(MainActivity.this, delete.class));
+                }
+            }
+
+        };
+
+        btnPost.setOnClickListener(listener);
+        btnPut.setOnClickListener(listener);
+        btnAll.setOnClickListener(listener);
+        btnDelete.setOnClickListener(listener);
+
     }
+
 }
